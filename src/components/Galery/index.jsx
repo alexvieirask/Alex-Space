ff99fa58194c13e56aca2e6e7e67e142584a74b0
alexvieirask <alexvieiradias2019@gmail.com>
2022-12-28 11:44:33 -0300

    updates

--- src/components/Galery/index.jsx
+++ src/components/Galery/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Tags from "../Tags";
 import Cards from "./Cards";
 import styles from "./galery.module.scss"
@@ -6,11 +7,22 @@ import pictures from "./pictures.json"
 
 
 export default function Galery(){
+    const [itens, setItens] = useState(pictures)
+    const tags = [...new Set(pictures.map(value => value.tag))]
+
+    function filterPicture(tag){
+        const newPictures = pictures.filter(picture => {
+            return picture.tag === tag
+        })
+
+        setItens(newPictures)
+    }
+
     return(
         <section className={styles.galery}>
             <h2>Browse the gallery</h2>
-            <Tags/>
-            <Cards itens={pictures} styles = {styles} />
+            <Tags tags={tags} filter={filterPicture} setItens= {setItens}/>
+            <Cards itens={itens} styles = {styles} />
         </section>
     )
 }
